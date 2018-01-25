5d1c24a2bc39b7816602fa7501a3946739a45517
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-01-26 00:51:12 +0100

    stworzenie skryptu

---
+++ js/poletrojkata.js
@@ -0,0 +1,4 @@
+var a = prompt('Podaj podstawe');
+var h = prompt('Podaj wysokosc');
+var poleTrojkata = a*h/2;
+alert('Pole trojkata o podstawie a: ' + a + ' i wysokosci h: ' + h + ' jest rowne: ' + poleTrojkata);
